ac44c21f080f6303ecc40ec8dac8de28343819d3
Nicolas Mora <github@babelouest.org>
2020-01-04 20:18:37 -0500

    Update doc

--- doc/html/search/all_0.js
+++ doc/html/search/all_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['constants',['Constants',['../group__const.html',1,'']]],
+  ['constants_20and_20properties',['Constants and properties',['../group__const.html',1,'']]],
   ['core_20functions',['Core functions',['../group__core.html',1,'']]]
 ];
--- doc/html/search/groups_3.js
+++ doc/html/search/groups_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['jwks_20functions',['JWKS functions',['../group__jwks.html',1,'']]],
   ['jwk_20type',['JWK type',['../group__type.html',1,'']]]
 ];

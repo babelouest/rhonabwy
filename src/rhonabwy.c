62be7043b97e536c490e3a79980f01ab9b866d13
Nicolas Mora <github@babelouest.org>
2020-01-27 23:18:43 -0500

    Return json_null() on *_export_to_json_t if input is NULL

--- src/rhonabwy.c
+++ src/rhonabwy.c
@@ -1105,7 +1105,7 @@ json_t * r_jwk_export_to_json_t(jwk_t * jwk) {
   if (jwk != NULL) {
     return json_deep_copy(jwk);
   } else {
-    return NULL;
+    return json_null();
   }
 }
 
@@ -1802,7 +1802,7 @@ json_t * r_jwks_export_to_json_t(jwks_t * jwks) {
   if (jwks != NULL) {
     return json_deep_copy(jwks);
   } else {
-    return NULL;
+    return json_null();
   }
 }
 

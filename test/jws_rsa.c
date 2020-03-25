e33ee1c8d17f906cb1c5c12fc2e0541b88c9e703
Nicolas Mora <github@babelouest.org>
2020-03-24 22:54:15 -0400

    Forgot to save this little one

--- test/jws_rsa.c
+++ test/jws_rsa.c
@@ -365,15 +365,31 @@ START_TEST(test_rhonabwy_set_alg_serialize_verify_ok)
   ck_assert_int_eq(r_jwk_import_from_json_str(jwk_privkey, jwk_privkey_rsa_no_alg_str), RHN_OK);
   ck_assert_int_eq(r_jwk_import_from_json_str(jwk_pubkey, jwk_pubkey_rsa_no_alg_str), RHN_OK);
   ck_assert_int_eq(r_jws_set_payload(jws_sign, (const unsigned char *)PAYLOAD, o_strlen(PAYLOAD)), RHN_OK);
+  
   ck_assert_int_eq(r_jws_set_alg(jws_sign, R_JWS_ALG_RS256), RHN_OK);
   ck_assert_int_eq(r_jws_add_keys(jws_sign, jwk_privkey, NULL), RHN_OK);
-
   ck_assert_ptr_ne((token = r_jws_serialize(jws_sign, NULL, 0)), NULL);
   
   ck_assert_int_eq(r_jws_parse(jws_verify, token, 0), RHN_OK);
   ck_assert_int_eq(r_jws_verify_signature(jws_verify, jwk_pubkey, 0), RHN_OK);
-  
   o_free(token);
+  
+  ck_assert_int_eq(r_jws_set_alg(jws_sign, R_JWS_ALG_RS384), RHN_OK);
+  ck_assert_int_eq(r_jws_add_keys(jws_sign, jwk_privkey, NULL), RHN_OK);
+  ck_assert_ptr_ne((token = r_jws_serialize(jws_sign, NULL, 0)), NULL);
+  
+  ck_assert_int_eq(r_jws_parse(jws_verify, token, 0), RHN_OK);
+  ck_assert_int_eq(r_jws_verify_signature(jws_verify, jwk_pubkey, 0), RHN_OK);
+  o_free(token);
+  
+  ck_assert_int_eq(r_jws_set_alg(jws_sign, R_JWS_ALG_RS512), RHN_OK);
+  ck_assert_int_eq(r_jws_add_keys(jws_sign, jwk_privkey, NULL), RHN_OK);
+  ck_assert_ptr_ne((token = r_jws_serialize(jws_sign, NULL, 0)), NULL);
+  
+  ck_assert_int_eq(r_jws_parse(jws_verify, token, 0), RHN_OK);
+  ck_assert_int_eq(r_jws_verify_signature(jws_verify, jwk_pubkey, 0), RHN_OK);
+  o_free(token);
+  
   r_jws_free(jws_sign);
   r_jws_free(jws_verify);
   r_jwk_free(jwk_privkey);

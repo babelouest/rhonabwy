4476500e3303f0a16c812f486a4cc1341bf36475
Nicolas Mora <github@babelouest.org>
2020-06-09 20:11:15 -0400

    Tell the world rnby implements alg A192KW and enc A192GCM with GnuTLS >= 3.6.14

--- src/misc.c
+++ src/misc.c
@@ -359,6 +359,10 @@ json_t * r_library_info_json_t() {
   json_array_append_new(json_object_get(json_object_get(j_info, "jws"), "alg"), json_string("PS384"));
   json_array_append_new(json_object_get(json_object_get(j_info, "jws"), "alg"), json_string("PS512"));
 #endif
+#if GNUTLS_VERSION_NUMBER >= 0x03060e
+  json_array_append_new(json_object_get(json_object_get(j_info, "jwe"), "alg"), json_string("A192KW"));
+  json_array_append_new(json_object_get(json_object_get(j_info, "jwe"), "enc"), json_string("A192GCM"));
+#endif
   return j_info;
 }
 

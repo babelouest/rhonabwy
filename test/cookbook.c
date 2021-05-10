f7a414ae81c2063acb9a64c79d9e8198c2b81eaa
Nicolas Mora <github@babelouest.org>
2021-05-10 09:12:43 -0400

    Fix jwe tests and allow some reproductible tests because I'm a better library now

--- test/cookbook.c
+++ test/cookbook.c
@@ -76,7 +76,7 @@ static void jws_test(const char * file_path) {
   ck_assert_ptr_ne(NULL, file_content = get_file_content(file_path));
   ck_assert_ptr_ne(NULL, j_content = json_loads(file_content, JSON_DECODE_ANY, NULL));
   
-  y_log_message(Y_LOG_LEVEL_DEBUG, "Run test: %s", json_string_value(json_object_get(j_content, "title")));
+  y_log_message(Y_LOG_LEVEL_INFO, "Run test: %s", json_string_value(json_object_get(j_content, "title")));
   
   ck_assert_int_eq(r_jwk_init(&jwk_privkey), RHN_OK);
   ck_assert_int_eq(r_jwk_init(&jwk_pubkey), RHN_OK);
@@ -112,17 +112,17 @@ static void jws_test(const char * file_path) {
 
 static void jwe_test(const char * file_path) {
   char * file_content, * token, ** token_split;
-  json_t * j_content, * j_header, * j_header_control;
+  json_t * j_content;
   jwe_t * jwe;
   jwk_t * jwk_privkey, * jwk_pubkey;
-  unsigned char iv[128] = {0}, cek[128] = {0}, header[1024] = {0};
+  unsigned char iv[128] = {0}, cek[128] = {0};
   const unsigned char * payload;
-  size_t iv_len = 0, cek_len = 0, payload_len = 0, header_len = 0;
+  size_t iv_len = 0, cek_len = 0, payload_len = 0;
   
   ck_assert_ptr_ne(NULL, file_content = get_file_content(file_path));
   ck_assert_ptr_ne(NULL, j_content = json_loads(file_content, JSON_DECODE_ANY, NULL));
   
-  y_log_message(Y_LOG_LEVEL_DEBUG, "Run test: %s", json_string_value(json_object_get(j_content, "title")));
+  y_log_message(Y_LOG_LEVEL_INFO, "Run test: %s", json_string_value(json_object_get(j_content, "title")));
   
   ck_assert_int_eq(r_jwk_init(&jwk_privkey), RHN_OK);
   if (json_object_get(json_object_get(j_content, "input"), "key") != NULL) {
@@ -137,33 +137,39 @@ static void jwe_test(const char * file_path) {
     ck_assert_ptr_ne(NULL, jwk_pubkey = r_jwk_copy(jwk_privkey));
   }
   
-  if (0 && json_object_get(j_content, "reproducible") == json_true()) { // TODO?
+  if (json_object_get(j_content, "reproducible") == json_true()) {
     ck_assert_int_eq(r_jwe_init(&jwe), RHN_OK);
     ck_assert_int_eq(r_jwe_set_payload(jwe, (const unsigned char *)json_string_value(json_object_get(json_object_get(j_content, "input"), "plaintext")), json_string_length(json_object_get(json_object_get(j_content, "input"), "plaintext"))), RHN_OK);
     ck_assert_int_eq(r_jwe_set_alg(jwe, r_str_to_jwa_alg(json_string_value(json_object_get(json_object_get(j_content, "input"), "alg")))), RHN_OK);
     ck_assert_int_eq(r_jwe_set_enc(jwe, r_str_to_jwa_enc(json_string_value(json_object_get(json_object_get(j_content, "input"), "enc")))), RHN_OK);
     ck_assert_int_eq(1, o_base64url_decode((const unsigned char *)json_string_value(json_object_get(json_object_get(j_content, "generated"), "iv")), json_string_length(json_object_get(json_object_get(j_content, "generated"), "iv")), iv, &iv_len));
     ck_assert_int_eq(r_jwe_set_iv(jwe, iv, iv_len), RHN_OK);
-    ck_assert_int_eq(1, o_base64url_decode((const unsigned char *)json_string_value(json_object_get(json_object_get(j_content, "generated"), "cek")), json_string_length(json_object_get(json_object_get(j_content, "generated"), "cek")), cek, &cek_len));
-    ck_assert_int_eq(r_jwe_set_cypher_key(jwe, cek, cek_len), RHN_OK);
+    if (json_object_get(json_object_get(j_content, "generated"), "cek") != NULL) {
+      ck_assert_int_eq(1, o_base64url_decode((const unsigned char *)json_string_value(json_object_get(json_object_get(j_content, "generated"), "cek")), json_string_length(json_object_get(json_object_get(j_content, "generated"), "cek")), cek, &cek_len));
+      ck_assert_int_eq(r_jwe_set_cypher_key(jwe, cek, cek_len), RHN_OK);
+    }
+    
+    // This stunt is executed by a trained professional, do not try this at home!!!
+    json_decref(jwe->j_header);
+    jwe->j_header = NULL;
+    jwe->j_header = json_copy(json_object_get(json_object_get(j_content, "encrypting_content"), "protected"));
+    // /stunt
+    
     ck_assert_ptr_ne(NULL, token = r_jwe_serialize(jwe, jwk_pubkey, 0));
     ck_assert_int_eq(5, split_string(token, ".", &token_split));
-    ck_assert_int_eq(1, o_base64url_decode((const unsigned char *)token_split[0], o_strlen(token_split[0]), header, &header_len));
-    header[header_len] = '\0';
-    ck_assert_ptr_ne(NULL, j_header = json_loads((const char *)header, JSON_DECODE_ANY, NULL));
-    ck_assert_int_eq(1, o_base64url_decode((const unsigned char *)json_string_value(json_object_get(json_object_get(json_object_get(j_content, "output"), "json_flat"), "protected")), json_string_length(json_object_get(json_object_get(json_object_get(j_content, "output"), "json_flat"), "protected")), header, &header_len));
-    header[header_len] = '\0';
-    ck_assert_ptr_ne(NULL, j_header_control = json_loads((const char *)header, JSON_DECODE_ANY, NULL));
-    ck_assert_int_eq(1, json_equal(j_header, j_header_control));
-    ck_assert_str_eq(token_split[1], json_string_value(json_object_get(json_object_get(json_object_get(j_content, "output"), "json_flat"), "encrypted_key")));
+    ck_assert_str_eq(token_split[0], json_string_value(json_object_get(json_object_get(json_object_get(j_content, "output"), "json_flat"), "protected")));
+    if (json_object_get(json_object_get(json_object_get(j_content, "output"), "json_flat"), "encrypted_key") != NULL) {
+      ck_assert_str_eq(token_split[1], json_string_value(json_object_get(json_object_get(json_object_get(j_content, "output"), "json_flat"), "encrypted_key")));
+    } else {
+      ck_assert_int_eq(0, o_strlen(token_split[1]));
+    }
     ck_assert_str_eq(token_split[2], json_string_value(json_object_get(json_object_get(json_object_get(j_content, "output"), "json_flat"), "iv")));
     ck_assert_str_eq(token_split[3], json_string_value(json_object_get(json_object_get(json_object_get(j_content, "output"), "json_flat"), "ciphertext")));
     ck_assert_str_eq(token_split[4], json_string_value(json_object_get(json_object_get(json_object_get(j_content, "output"), "json_flat"), "tag")));
     ck_assert_str_eq(token, json_string_value(json_object_get(json_object_get(j_content, "output"), "compact")));
     r_jwe_free(jwe);
     o_free(token);
-    json_decref(j_header);
-    json_decref(j_header_control);
+    free_string_array(token_split);
   }
   
   ck_assert_int_eq(r_jwe_init(&jwe), RHN_OK);
@@ -185,7 +191,9 @@ START_TEST(test_rhonabwy_cookbook_jwk)
   char * file_content;
   jwk_t * jwk;
   unsigned int bits = 0;
+  y_log_message(Y_LOG_LEVEL_INFO, "Test JWK");
   
+  y_log_message(Y_LOG_LEVEL_INFO, "Run test: %s", o_strrchr(JSON_JWK_3_1_EC_PUBLIC, '/')+1);
   ck_assert_ptr_ne(NULL, file_content = get_file_content(JSON_JWK_3_1_EC_PUBLIC));
   ck_assert_int_eq(r_jwk_init(&jwk), RHN_OK);
   ck_assert_int_eq(r_jwk_import_from_json_str(jwk, file_content), RHN_OK);
@@ -194,6 +202,7 @@ START_TEST(test_rhonabwy_cookbook_jwk)
   r_jwk_free(jwk);
   o_free(file_content);
   
+  y_log_message(Y_LOG_LEVEL_INFO, "Run test: %s", o_strrchr(JSON_JWK_3_2_EC_PRIVATE, '/')+1);
   ck_assert_ptr_ne(NULL, file_content = get_file_content(JSON_JWK_3_2_EC_PRIVATE));
   ck_assert_int_eq(r_jwk_init(&jwk), RHN_OK);
   ck_assert_int_eq(r_jwk_import_from_json_str(jwk, file_content), RHN_OK);
@@ -202,6 +211,7 @@ START_TEST(test_rhonabwy_cookbook_jwk)
   r_jwk_free(jwk);
   o_free(file_content);
   
+  y_log_message(Y_LOG_LEVEL_INFO, "Run test: %s", o_strrchr(JSON_JWK_3_3_RSA_PUBLIC, '/')+1);
   ck_assert_ptr_ne(NULL, file_content = get_file_content(JSON_JWK_3_3_RSA_PUBLIC));
   ck_assert_int_eq(r_jwk_init(&jwk), RHN_OK);
   ck_assert_int_eq(r_jwk_import_from_json_str(jwk, file_content), RHN_OK);
@@ -210,6 +220,7 @@ START_TEST(test_rhonabwy_cookbook_jwk)
   r_jwk_free(jwk);
   o_free(file_content);
   
+  y_log_message(Y_LOG_LEVEL_INFO, "Run test: %s", o_strrchr(JSON_JWK_3_4_RSA_PRIVATE, '/')+1);
   ck_assert_ptr_ne(NULL, file_content = get_file_content(JSON_JWK_3_4_RSA_PRIVATE));
   ck_assert_int_eq(r_jwk_init(&jwk), RHN_OK);
   ck_assert_int_eq(r_jwk_import_from_json_str(jwk, file_content), RHN_OK);
@@ -218,6 +229,7 @@ START_TEST(test_rhonabwy_cookbook_jwk)
   r_jwk_free(jwk);
   o_free(file_content);
   
+  y_log_message(Y_LOG_LEVEL_INFO, "Run test: %s", o_strrchr(JSON_JWK_3_5_SYMMETRIC_MAC, '/')+1);
   ck_assert_ptr_ne(NULL, file_content = get_file_content(JSON_JWK_3_5_SYMMETRIC_MAC));
   ck_assert_int_eq(r_jwk_init(&jwk), RHN_OK);
   ck_assert_int_eq(r_jwk_import_from_json_str(jwk, file_content), RHN_OK);
@@ -226,6 +238,7 @@ START_TEST(test_rhonabwy_cookbook_jwk)
   r_jwk_free(jwk);
   o_free(file_content);
   
+  y_log_message(Y_LOG_LEVEL_INFO, "Run test: %s", o_strrchr(JSON_JWK_3_6_SYMMETRIC_ENC, '/')+1);
   ck_assert_ptr_ne(NULL, file_content = get_file_content(JSON_JWK_3_6_SYMMETRIC_ENC));
   ck_assert_int_eq(r_jwk_init(&jwk), RHN_OK);
   ck_assert_int_eq(r_jwk_import_from_json_str(jwk, file_content), RHN_OK);
@@ -238,6 +251,7 @@ END_TEST
 
 START_TEST(test_rhonabwy_cookbook_jws)
 {
+  y_log_message(Y_LOG_LEVEL_INFO, "Test JWS");
   jws_test(JSON_JWS_4_1_RSA_V15);
 #if GNUTLS_VERSION_NUMBER >= 0x030600
   jws_test(JSON_JWS_4_2_RSA_PSS);
@@ -250,7 +264,8 @@ END_TEST
 
 START_TEST(test_rhonabwy_cookbook_jwe)
 {
-  jwe_test(JSON_JWE_5_1_RSA_V15_AES_CBC);
+  y_log_message(Y_LOG_LEVEL_INFO, "Test JWE");
+  jwe_test(JSON_JWE_5_1_RSA_V15_AES_CBC); // Not reproductible yet
 #if NETTLE_VERSION_NUMBER >= 0x030400
   jwe_test(JSON_JWE_5_2_RSA_OAEP_AES_GCM);
 #endif

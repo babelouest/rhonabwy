cbef92a1260230931589cfd9daf3ef8e0462d8eb
Nicolas Mora <github@babelouest.org>
2022-12-31 18:07:32 -0500

    Small improvements on r_jwt_validate_claims for date-based values

--- src/jwt.c
+++ src/jwt.c
@@ -1756,7 +1756,7 @@ int r_jwt_validate_claims(jwt_t * jwt, ...) {
           i_value = va_arg(vl, int);
           if (i_value == R_JWT_CLAIM_PRESENT && !json_is_integer(json_object_get(jwt->j_claims, "exp"))) {
             ret = RHN_ERROR_PARAM;
-          } else if (json_is_integer(json_object_get(jwt->j_claims, "exp"))) {
+          } else if (json_is_integer(json_object_get(jwt->j_claims, "exp")) && json_integer_value(json_object_get(jwt->j_claims, "exp")) > 0) {
             t_value = (time_t)r_jwt_get_claim_int_value(jwt, "exp");
             if (i_value == R_JWT_CLAIM_NOW) {
               if (t_value < now) {
@@ -1775,7 +1775,7 @@ int r_jwt_validate_claims(jwt_t * jwt, ...) {
           i_value = va_arg(vl, int);
           if (i_value == R_JWT_CLAIM_PRESENT && !json_is_integer(json_object_get(jwt->j_claims, "nbf"))) {
             ret = RHN_ERROR_PARAM;
-          } else if (json_is_integer(json_object_get(jwt->j_claims, "nbf"))) {
+          } else if (json_is_integer(json_object_get(jwt->j_claims, "nbf")) && json_integer_value(json_object_get(jwt->j_claims, "nbf")) > 0) {
             t_value = (time_t)r_jwt_get_claim_int_value(jwt, "nbf");
             if (i_value == R_JWT_CLAIM_NOW) {
               if (t_value > now) {
@@ -1794,7 +1794,7 @@ int r_jwt_validate_claims(jwt_t * jwt, ...) {
           i_value = va_arg(vl, int);
           if (i_value == R_JWT_CLAIM_PRESENT && !json_is_integer(json_object_get(jwt->j_claims, "iat"))) {
             ret = RHN_ERROR_PARAM;
-          } else if (json_is_integer(json_object_get(jwt->j_claims, "iat"))) {
+          } else if (json_is_integer(json_object_get(jwt->j_claims, "iat")) && json_integer_value(json_object_get(jwt->j_claims, "iat")) > 0) {
             t_value = (time_t)r_jwt_get_claim_int_value(jwt, "iat");
             if (i_value == R_JWT_CLAIM_NOW) {
               if (t_value > now) {

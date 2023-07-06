fa9a09817bb1573b873fcfcb4ede00b902c91f67
ksivask <ksivask@gmail.com>
2023-07-06 07:28:14 -0700

    address review comments: var decl, error check

--- src/jwe.c
+++ src/jwe.c
@@ -695,6 +695,8 @@ static int _r_concat_kdf(jwe_t * jwe, jwa_alg alg, const gnutls_datum_t * Z, gnu
              * apu = r_jwe_get_header_str_value(jwe, "apu"),
              * apv = r_jwe_get_header_str_value(jwe, "apv");
   size_t alg_id_len = o_strlen(alg_id), key_data_len = 0;
+  size_t derived_key_len = _r_get_key_size(jwe->enc);
+  size_t current_key_len = 0;
 
   kdf->data = NULL;
   kdf->size = 0;
@@ -795,9 +797,6 @@ static int _r_concat_kdf(jwe_t * jwe, jwa_alg alg, const gnutls_datum_t * Z, gnu
     kdf->data[kdf->size+3] = (unsigned char)(key_data_len) & 0xFF;
     kdf->size += 4;
 
-    size_t derived_key_len = _r_get_key_size(jwe->enc);
-    size_t current_key_len = 0;
-
     for (uint8_t i = 1; ; i++) {
       memset(kdf->data+3, i, 1);
       if (gnutls_hash_fast(GNUTLS_DIG_SHA256, kdf->data, kdf->size, derived_key+current_key_len) != GNUTLS_E_SUCCESS) {
@@ -809,6 +808,9 @@ static int _r_concat_kdf(jwe_t * jwe, jwa_alg alg, const gnutls_datum_t * Z, gnu
       if (alg != R_JWA_ALG_ECDH_ES || current_key_len >= derived_key_len) {
         break;
       }
+    }
+    if (ret != RHN_OK) {
+      break;
     }
 
   } while (0);

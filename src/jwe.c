f273a3d5e59113039535d4d9a357105d26ddef15
ksivask <ksivask@gmail.com>
2024-05-03 23:52:35 -0700

    use correct padding logic for block ciphers

--- src/jwe.c
+++ src/jwe.c
@@ -2259,7 +2259,7 @@ static void r_jwe_remove_padding(unsigned char * text, size_t * text_len, unsign
   unsigned char pad = text[(*text_len)-1], i;
   int pad_ok = 1;
 
-  if (pad && pad < (unsigned char)block_size) {
+  if (pad && pad <= (unsigned char)block_size) {
     for (i=0; i<pad; i++) {
       if (text[((*text_len)-i-1)] != pad) {
         pad_ok = 0;
